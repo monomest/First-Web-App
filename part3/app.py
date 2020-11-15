309754611ba20d32dc6ea678a530b612f1d5303d
Renee Lu <reneeylu29@gmail.com>
2020-11-15 13:47:34 +1100

    Updates to About, Contact and Students

--- part3/app.py
+++ part3/app.py
@@ -17,6 +17,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/api/studentlist' ,methods=['GET', 'POST'])
 def studentlist():
     with open('../part1/student-data.json') as f:
